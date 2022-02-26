aed190f769ac0c162e11166c410db589eba0d422
im-vaibhav <va8817@gmail.com>
2022-02-25 23:51:12 -0800

    fix client code

--- internal/client.go
+++ internal/client.go
@@ -4,27 +4,21 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"net/http/httptrace"
 	"time"
 )
 
 func Client(w http.ResponseWriter, r *http.Request) {
 	log.Println("[CLIENT] running now!")
 
-	clientTrace := &httptrace.ClientTrace{
-		GotConn: func(info httptrace.GotConnInfo) { log.Printf("[CLIENT] conn was reused: %t\n", info.Reused) },
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:9001/", nil)
+	if err != nil {
+		log.Printf("[CLIENT] %v", err)
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Millisecond*80))
 	defer cancel()
-	traceCtx := httptrace.WithClientTrace(ctx, clientTrace)
-
-	// 1st request
-	req, _ := http.NewRequestWithContext(traceCtx, http.MethodGet, "http://localhost:9001", nil)
-	_, _ = http.DefaultClient.Do(req)
-
-	// 2nd request
-	req, _ = http.NewRequestWithContext(traceCtx, http.MethodGet, "http://localhost:9001", nil)
-	_, _ = http.DefaultClient.Do(req)
+	req = req.WithContext(ctx)
+	c := &http.Client{}
+	_, _ = c.Do(req)
 
 	log.Println("[CLIENT] ran succesfully")
 }
